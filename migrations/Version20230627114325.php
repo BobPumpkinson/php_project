88e8954cb93003ccda1bc57566b765384d9fc1e0
BobPumpkinson <106379137+BobPumpkinson@users.noreply.github.com>
2023-06-27 22:53:06 +0200

    Add files via upload

---
+++ migrations/Version20230627114325.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230627114325 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_5F9E962AE7927C74 ON comments');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_5F9E962AE7927C74 ON comments (email)');
+    }
+}
